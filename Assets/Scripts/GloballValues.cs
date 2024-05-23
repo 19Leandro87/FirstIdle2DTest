62e0070de82fd3807c4780f8ab47c86c2574e42c
Leandro87 <lollisarts@gmail.com>
2024-05-23 13:20:06 +0200

    minor code changes, starting to setup units references

--- Assets/Scripts/GloballValues.cs
+++ Assets/Scripts/GloballValues.cs
@@ -1,6 +1,10 @@
 public class GlobalValues
 {
-    public static float BASE_POLLUTION = 100000.0f;
-    public static float UNIT1_BASE_PRICE = 50.0f;
-    public static float UNIT1_BASE_POLLUTION_CLEAN = 1f;
+    public static readonly float BASE_POLLUTION = 100000.0f;
+
+    public static readonly float UNIT1_BASE_PRICE = 50.0f;
+    public static readonly float UNIT1_BASE_POLLUTION_CLEAN = 1f;
+
+    public static readonly float UNIT2_BASE_PRICE = 250.0f;
+    public static readonly float UNIT2_BASE_POLLUTION_CLEAN = 100f;
 }
--- Assets/Scripts/UnitsManager.cs
+++ Assets/Scripts/UnitsManager.cs
@@ -6,17 +6,28 @@ using UnityEngine.UI;
 
 public class UnitsManager : MonoBehaviour
 {
+    public static UnitsManager Instance { get; private set; }
+
+    private int buyMultiplier;
+
+    //---UNIT 1---
     [SerializeField] private TextMeshProUGUI unit1CostText;
     private float unit1Cost;
     [SerializeField] private TextMeshProUGUI unit1CountText;
-    private float unit1Count;
+    private int unit1Count;
+    private float unit1PollutionClean;
     [SerializeField] private Button unit1BuyButton;
 
     private void Awake() {
+        Instance = this;
+        buyMultiplier = 1;
+
+        //---UNIT 1---
         unit1Cost = GlobalValues.UNIT1_BASE_PRICE;
         unit1CostText.text = "$ " + unit1Cost;
         unit1Count = 0;
         unit1CountText.text = "N. " + unit1Count.ToString();
+        unit1PollutionClean = GlobalValues.UNIT1_BASE_POLLUTION_CLEAN;
     }
 
     private void Start() {
@@ -24,5 +35,12 @@ public class UnitsManager : MonoBehaviour
 
     }
 
+    private void UpdateUnitLine(int multiplier, float presentCost, float presentPollutionClean, int presentCount) {
+        
+    }
+
+    public void ChangeBuyMultiplier(int multiplier) { 
+        buyMultiplier = multiplier; 
+    }
 
 }
