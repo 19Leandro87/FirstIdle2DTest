81deb2290b67d3d8518187f90bad31240aeb3bcf
Leandro87 <lollisarts@gmail.com>
2024-05-29 13:54:40 +0200

    working on the unit management, identifying scroll menu lines and unit fields

--- Assets/Scripts/GloballValues.cs
+++ Assets/Scripts/GloballValues.cs
@@ -5,16 +5,8 @@ public class GlobalValues
     public static readonly float BASE_POLLUTION = 100000.0f;
 
     public static readonly List<UnitObject> BASE_UNITS = new List<UnitObject> { 
-        new UnitObject { Enabled = false, Name = "Unit 0", Level = 0, Price = 50f, PriceFactor = 1.5f, PollutionClean = 5f, PollutionCleanFactor = 1.2f },
+        new UnitObject { Enabled = true, Name = "Unit 0", Level = 0, Price = 50f, PriceFactor = 1.5f, PollutionClean = 5f, PollutionCleanFactor = 1.2f },
         new UnitObject { Enabled = false, Name = "Unit 1", Level = 0, Price = 50f, PriceFactor = 1.5f, PollutionClean = 5f, PollutionCleanFactor = 1.2f },
-        new UnitObject { Enabled = false, Name = "Unit 2", Level = 0, Price = 50f, PriceFactor = 1.5f, PollutionClean = 5f, PollutionCleanFactor = 1.2f },
-        new UnitObject { Enabled = false, Name = "Unit 3", Level = 0, Price = 50f, PriceFactor = 1.5f, PollutionClean = 5f, PollutionCleanFactor = 1.2f },
-        new UnitObject { Enabled = false, Name = "Unit 4", Level = 0, Price = 50f, PriceFactor = 1.5f, PollutionClean = 5f, PollutionCleanFactor = 1.2f },
-        new UnitObject { Enabled = false, Name = "Unit 5", Level = 0, Price = 50f, PriceFactor = 1.5f, PollutionClean = 5f, PollutionCleanFactor = 1.2f },
-        new UnitObject { Enabled = false, Name = "Unit 6", Level = 0, Price = 50f, PriceFactor = 1.5f, PollutionClean = 5f, PollutionCleanFactor = 1.2f },
-        new UnitObject { Enabled = false, Name = "Unit 7", Level = 0, Price = 50f, PriceFactor = 1.5f, PollutionClean = 5f, PollutionCleanFactor = 1.2f },
-        new UnitObject { Enabled = false, Name = "Unit 8", Level = 0, Price = 50f, PriceFactor = 1.5f, PollutionClean = 5f, PollutionCleanFactor = 1.2f },
-        new UnitObject { Enabled = false, Name = "Unit 9", Level = 0, Price = 50f, PriceFactor = 1.5f, PollutionClean = 5f, PollutionCleanFactor = 1.2f },
-        new UnitObject { Enabled = false, Name = "Unit 10", Level = 0, Price = 50f, PriceFactor = 1.5f, PollutionClean = 5f, PollutionCleanFactor = 1.2f }
+        new UnitObject { Enabled = false, Name = "Unit 2", Level = 0, Price = 50f, PriceFactor = 1.5f, PollutionClean = 5f, PollutionCleanFactor = 1.2f }
     };
 }
--- Assets/Scripts/UnitsManager.cs
+++ Assets/Scripts/UnitsManager.cs
@@ -8,26 +8,52 @@ public class UnitsManager : MonoBehaviour
 {
     public static UnitsManager Instance { get; private set; }
 
-    [SerializeField] private List<UnitLineObject> unitLines;
-
-    private int buyMultiplier;
+    [SerializeField] private List<GameObject> unitLines;
+    [SerializeField] private List<UnitLineObject> unitDataFields;
     private List<UnitObject> units;
+    private int buyMultiplier;
+
+
+    /*
+     COST PROGRESSION HYPOTHESIS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    cost_{next} = cost_{base} * (pricefactor)^{level}
+
+     */
 
     private void Awake() {
         Instance = this;
         buyMultiplier = 1;
+        units = new List<UnitObject>();
+
+        //Add a basic unit for each existing unit line to the units list, then if there's a save game update them all
+        for (int i = 0; i < unitLines.Count; i++) {
+            units.Add(GlobalValues.BASE_UNITS[i]);
+            if (SaveSystem.SaveGamesExist()) {
+                unitLines[i].gameObject.SetActive(JsonUtility.FromJson<SaveObject>(SaveSystem.Load()).unitsEnabled[i]);
+                units[i].Level = JsonUtility.FromJson<SaveObject>(SaveSystem.Load()).unitsLevel[i];
+
+                if (units[i].Level > 0)
+                    units[i].Price = GlobalValues.BASE_UNITS[i].Price * Mathf.Pow(GlobalValues.BASE_UNITS[i].PriceFactor, units[i].Level);
+                else
+                    units[i].Price = GlobalValues.BASE_UNITS[i].Price;
+
+                unitDataFields[i].priceText.text = units[i].Price.ToString();
+                unitDataFields[i].levelText.text = units[i].Level.ToString();
+            }
+        }
     }
 
     private void Start() {
-        units = new List<UnitObject>();
+
+
         units.Add(GlobalValues.BASE_UNITS[0]);
 
         units[0].Price = 100;
-        unitLines[0].priceText.text = units[0].Price.ToString();
+        //unitLines[0].priceText.text = units[0].Price.ToString();
 
-        unitLines[0].buyButton.onClick.AddListener(() => {
+        /*unitLines[0].buyButton.onClick.AddListener(() => {
             unitLines[0].priceText.text = (GlobalValues.BASE_UNITS[0].PriceFactor * units[0].Price).ToString();
-        });
+        });*/
 
     }
 
